333c251a758d4962e6790a1fba1a5294c1d795af
Matthew Heun <matthew.heun@me.com>
2024-02-08 11:31:09 -0500

    First running pipeline

---
+++ R/pipeline.R
@@ -0,0 +1,44 @@
+#' Create the targets pipeline for the CL-PFU database
+#'
+#' This is a type of target factory whose arguments
+#' specify the details of a targets workflow to be constructed
+#'
+#' `conn_params` should be a named list containing some information for the connection.
+#'
+#' * dbname: a string identifying the name of the database
+#' * user: the username of the person connecting
+#' * host: the hostname or IP address of the database management system (DBMS)
+#' * port: the port for DBMS connections
+#'
+#' The passwords should _not_ be included in `conn_params` and will be ignored.
+#' Rather, passwords should be set in the user's `.pgpass` file.
+#'
+#' @param input_data_version A string indicating which input data should be used.
+#' @param output_version A string indicating the output version.
+#' @param conn_params A named list containing connection parameters to the database.
+#'                    See details.
+#' @param countries A vector of countries to be analyzed.
+#' @param years A vector of years to be analyzed.
+#' @param do_chops A boolean that tells whether to perform slices on the
+#'                 resource matrix and final demand matrix.
+#'
+#' @return A list representing the `targets` pipeline
+#'
+#' @export
+get_pipeline <- function(input_data_version,
+                         output_version,
+                         conn_params,
+                         countries,
+                         years,
+                         do_chops) {
+
+  setup <- PFUSetup::get_abs_paths(version = input_data_version)
+
+
+  # Return the targets pipeline
+  list(
+    tar_target(Schema,
+               PFUPipelineTools::load_schema_table(version = input_data_version))
+
+  )
+}
---
+++ _targets.R
@@ -0,0 +1,79 @@
+# targets::tar_make() to run the pipeline
+# targets::tar_make_future(workers = 8) to execute across multiple cores.
+# targets::tar_read(<<target_name>>) to view the results.
+# PFUPipelineTools::pl_destroy() to start over.
+# targets::tar_make(callr_function = NULL) to debug.
+
+
+##############################
+# User-adjustable parameters #
+##############################
+
+countries <- c(PFUPipelineTools::canonical_countries, "WRLD") |> as.character()
+years <- 1960:2020
+do_chops <- FALSE
+
+input_data_version <- "v1.4"
+output_version <- "v1.4a1"
+
+db_user <- "mkh2"
+db_host <- "eviz.cs.calvin.edu"
+
+n_worker_threads <- 16 # For parallel processing
+
+
+##################################
+# End user-adjustable parameters #
+##################################
+
+# Set database connection parameters
+conn_params <- list(dbname = output_version,
+                    user = db_user,
+                    host = db_host,
+                    port = 5432)
+
+# Load packages required to define the pipeline:
+library(PFUPipeline2)
+library(targets)
+library(qs)
+# library(tarchetypes) # Load other packages as needed.
+
+# Set target options:
+tar_option_set(
+  # packages that your targets need to run
+  packages = c("Matrix",
+               "PFUPipelineTools",
+               "tibble"),
+  # Optionally set the default storage format. qs is fast.
+  format = "qs",
+
+  # For distributed computing in tar_make(), supply a {crew} controller
+  # as discussed at https://books.ropensci.org/targets/crew.html.
+  # Choose a controller that suits your needs. For example, the following
+  # sets a controller with 2 workers which will run as local R processes:
+
+  controller = crew::crew_controller_local(workers = n_worker_threads)
+
+  # Alternatively, if you want workers to run on a high-performance computing
+  # cluster, select a controller from the {crew.cluster} package. The following
+  # example is a controller for Sun Grid Engine (SGE).
+
+  #   controller = crew.cluster::crew_controller_sge(
+  #     workers = 50,
+  #     # Many clusters install R as an environment module, and you can load it
+  #     # with the script_lines argument. To select a specific verison of R,
+  #     # you may need to include a version string, e.g. "module load R/4.3.0".
+  #     # Check with your system administrator if you are unsure.
+  #     script_lines = "module load R"
+  #   )
+
+  # Set other options as needed.
+)
+
+# Get the target list
+PFUPipeline2::get_pipeline(input_data_version = input_data_version,
+                           output_version = output_version,
+                           conn_params = conn_params,
+                           countries = countries,
+                           years = years,
+                           do_chops = do_chops)
