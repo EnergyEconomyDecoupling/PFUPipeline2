1842fdc6ca12f52ff387e943c3c1949a10bdde31
MatthewHeun <matthew.heun@me.com>
2024-02-12 21:26:15 -0500

    Working on pipeline

--- pipeline.R
+++ pipeline.R
@@ -3,22 +3,23 @@
 
 list(
 
-  # SchemaFilePath -------------------------------------------------------------
-  # Set as a target to track changes.
+  # Schema and data model ------------------------------------------------------
+
+  ## SchemaFilePath
   targets::tar_target_raw(
     "SchemaFilePath",
-    PFUSetup::get_abs_paths(version = input_data_version)[["schema_path"]],
+    pfu_setup_paths[["schema_path"]],
     format = "file"
   ),
 
-  # SchemaTable ----------------------------------------------------------------
+  ## SchemaTable
   # Load the schema table from the schema file.
   targets::tar_target(
     SchemaTable,
     PFUPipelineTools::load_schema_table(schema_path = SchemaFilePath)
   ),
 
-  # SimpleFKTables -------------------------------------------------------------
+  ## SimpleFKTables
   # Store a list of foreign key tables available in SchemaFilePath.
   # Additions will be made later.
   targets::tar_target(
@@ -26,14 +27,14 @@ list(
     PFUPipelineTools::load_simple_tables(simple_tables_path = SchemaFilePath)
   ),
 
-  # DM -------------------------------------------------------------------------
+  ## DM
   # Create the data model (dm object) from the SchemaTable.
   targets::tar_target(
     DM,
     PFUPipelineTools::schema_dm(SchemaTable)
   ),
 
-  # UploadDM -------------------------------------------------------------------
+  ## UploadDM
   # Upload the data model and SimpleFKTables to the database.
   targets::tar_target(
     UploadDM,
@@ -41,12 +42,53 @@ list(
                                                          simple_tables = SimpleFKTables,
                                                          conn = conn,
                                                          drop_db_tables = TRUE)
-  )
+  ),
+
+
+  # Country concordance --------------------------------------------------------
+
+  ## CountryConcordancePath
+  targets::tar_target_raw(
+    "CountryConcordancePath",
+    pfu_setup_paths[["country_concordance_path"]],
+    format = "file"
+  ),
+
+  ## CountryConcordanceTable
+  targets::tar_target(
+    CountryConcordanceTable,
+    load_country_concordance_table(country_concordance_path = CountryConcordancePath)
+  ),
+
+
+  # IEA data -------------------------------------------------------------------
+
+  ## IEADataPath
+  targets::tar_target_raw(
+    "IEADataPath",
+    pfu_setup_paths[["iea_data_path"]],
+    format = "file"
+  ),
+
+  ## AllIEAData
+  targets::tar_target(
+    AllIEAData,
+    IEATools::load_tidy_iea_df(IEADataPath,
+                               override_df = CountryConcordanceTable,
+                               specify_non_energy_flows = TRUE,
+                               apply_fixes = TRUE))
+
+  ## Upload AllIEAData
+
 
 ) |>
-  # Ensure each target has access to the database,
-  # using the hint found at https://github.com/ropensci/targets/discussions/1164.
+
+
+  # Hook before for conn -------------------------------------------------------
+
   tarchetypes::tar_hook_before(hook = {
+    # Ensure each target has access to the database,
+    # using the hint found at https://github.com/ropensci/targets/discussions/1164.
     conn <- DBI::dbConnect(drv = RPostgres::Postgres(),
                            dbname = conn_params$dbname,
                            host = conn_params$host,
