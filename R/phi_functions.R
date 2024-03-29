3e8b91d6bdb291b3880c2c69d0f9795acb55505e
Matthew Heun <matthew.heun@me.com>
2024-03-29 10:24:12 -0400

    Better default args

--- R/phi_functions.R
+++ R/phi_functions.R
@@ -310,6 +310,13 @@ assemble_phi_u_tables <- function(incomplete_phi_u_table,
     ) |>
     PFUPipelineTools::pl_upsert(in_place = TRUE,
                                 db_table_name = db_table_name,
+                                # We need to keep the table name
+                                additional_hash_group_cols = c(PFUPipelineTools::hashed_table_colnames$db_table_name,
+                                                               PFUPipelineTools::additional_hash_group_cols),
+                                # Don't keep single unique columns,
+                                # because groups may have different columns
+                                # with single unique values.
+                                keep_single_unique_cols = FALSE,
                                 conn = conn,
                                 schema = schema,
                                 fk_parent_tables = fk_parent_tables)
