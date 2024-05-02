594edcc3d23d76190b8c367904d0bc3139e81a70
Matthew Heun <matthew.heun@me.com>
2024-05-02 15:53:09 -0400

    Added graph code for the paper

--- R/stats.R
+++ R/stats.R
@@ -21,3 +21,44 @@ calc_db_coverage_stats <- function(completed_allcoation_tables,
     )
 
 }
+
+
+
+
+# Graph code for the paper
+
+
+# counts <- dplyr::bind_rows(dplyr::count(dplyr::group_by(stats, Country), CsourceType),
+#                            dplyr::count(stats, CsourceType) |>
+#                              dplyr::mutate(Country = "World")) |>
+#   tidyr::pivot_wider(names_from = CsourceType, values_from = n) |>
+#   tidyr::replace_na(replace = list(WRLD = 0, exemplar = 0, own = 0)) |>
+#   dplyr::mutate(
+#     table = "CompletedAllocationTables"
+#   )
+#
+# h_size <- 2
+#
+# counts |>
+#   dplyr::filter(Country %in% c("GHA", "GBR", "USA", "ZAF")) |>
+#   tidyr::pivot_longer(cols = c(WRLD, exemplar, own),
+#                       names_to = "var",
+#                       values_to = "val") |>
+#   dplyr::mutate(
+#     var = factor(var, levels = c("own", "exemplar", "WRLD")),
+#     x = h_size
+#   ) |>
+#   ggplot2::ggplot(mapping = ggplot2::aes(x = x, y = val, fill = var)) +
+#   ggplot2::geom_col(color = "black") +
+#   ggplot2::geom_text(ggplot2::aes(label = val),
+#                      position = ggplot2::position_stack(vjust = 0.5),
+#                      size = 3) +
+#   ggplot2::coord_polar(theta = "y", direction = -1) +
+#   ggplot2::xlim(c(0.2, h_size + 0.5)) +
+#   ggplot2::facet_wrap(facets = "Country", scales = "free_y") +
+#   ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white"),
+#                  panel.grid = ggplot2::element_blank(),
+#                  axis.title = ggplot2::element_blank(),
+#                  axis.ticks = ggplot2::element_blank(),
+#                  axis.text = ggplot2::element_blank())
+#
