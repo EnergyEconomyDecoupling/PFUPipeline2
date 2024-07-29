68c0573ebd25f00140fca454cc8dc84145d1d762
Matthew Heun <matthew.heun@me.com>
2024-07-29 10:32:01 -0400

    Delete commented lines

--- _pl_setup.R
+++ _pl_setup.R
@@ -76,10 +76,6 @@ if (parallel::detectCores() == 10) {
 # For debugging in a single thread. Also set callr_function = NULL.
 # crew_controller <- NULL
 # For parallel processing.
-# crew_controller <- crewargs::crewargs_controller_local_args(
-#   workers = worker_threads,
-#   seconds_idle = 60,
-#   cmdargs = "--max-connections=512")
 crew_controller <- crew::crew_controller_local(
   workers = worker_threads,
   seconds_idle = 60,
