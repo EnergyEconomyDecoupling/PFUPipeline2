8274459f82e9d5bcabdf90dd8e95173fb5abd9aa
Matthew Heun <matthew.heun@me.com>
2024-08-01 10:41:27 -0400

    IndexMap preserves correct rows

--- R/schema.R
+++ R/schema.R
@@ -53,6 +53,12 @@ set_dm_fk_tables <- function(schema_file_path, reset_schema = FALSE, conn) {
 #' @param fk_parent_tables A named list of all parent tables
 #'                         for the foreign keys in `db_table_name`.
 #'                         See details.
+#' @param index_colname The name of the index column.
+#'                      Default is `index_table_name`.
+#' @param pk_suffix The suffix for primary key columns.
+#'                  This suffix is attached to `index_colname` to create the
+#'                  name of the primary key for the index map.
+#'                  Default is `PFUPipelineTools::key_col_info$pk_suffix`.
 #'
 #' @return A named list appropriate to be an
 #'         index map for this database.
@@ -60,11 +66,16 @@ set_dm_fk_tables <- function(schema_file_path, reset_schema = FALSE, conn) {
 create_index_map <- function(index_table_name = "Index",
                              conn,
                              schema = PFUPipelineTools::schema_from_conn(conn),
-                             fk_parent_tables = PFUPipelineTools::get_all_fk_tables(conn = conn, schema = schema)) {
+                             fk_parent_tables = PFUPipelineTools::get_all_fk_tables(conn = conn, schema = schema),
+                             index_colname = index_table_name,
+                             pk_suffix = PFUPipelineTools::key_col_info$pk_suffix) {
 
+  index_id_colname <- paste0(index_table_name, pk_suffix)
   index_table <- index_table_name |>
     PFUPipelineTools::pl_filter_collect(collect = TRUE,
                                         conn = conn,
                                         schema = schema,
-                                        fk_parent_tables = fk_parent_tables)
+                                        fk_parent_tables = fk_parent_tables) |>
+    # Keep only the index_colname and and index_id_colname
+    dplyr::select(dplyr::all_of(c(index_id_colname, index_colname)))
 }
