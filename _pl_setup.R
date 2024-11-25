782a3b8a7332d455d26829494af62af602989699
Matthew Heun <matthew.heun@me.com>
2024-11-25 13:31:05 -0500

    Updates for adding regional aggs to details mats

--- _pl_setup.R
+++ _pl_setup.R
@@ -8,14 +8,14 @@
 
 # Countries --------------------------------------------------------------------
 
-countries <- c(PFUPipelineTools::canonical_countries, "WRLD") |> as.character()
+# countries <- c(PFUPipelineTools::canonical_countries, "WRLD") |> as.character()
 # countries <- "AGO"
 # countries <- "BEN"
 # countries <- "GHA"
 # countries <- "USA"
 # countries <- "CMR"
 # countries <- "WRLD"
-# countries <- c("GHA", "ZAF")
+countries <- c("GHA", "ZAF")
 # countries <- c("USA", "WRLD")
 
 #
@@ -24,12 +24,12 @@ countries <- c(PFUPipelineTools::canonical_countries, "WRLD") |> as.character()
 # Years ------------------------------------------------------------------------
 
 # Set the years for IEA data analysis
-years <- 1960:2020
+# years <- 1960:2020
 # years <- 1960
 # years <- 1960:1980
 # years <- 1995:2020
 # years <- 1995:1996
-# years <- 1971:1972
+years <- 1971:1972
 # years <- 1971:1980
 # years <- 1995
 # years <- 1971
@@ -87,7 +87,6 @@ crew_controller <- crew::crew_controller_local(
 # Dataset and version information ----------------------------------------------
 
 # Tells what database you are targeting
-# dbname <- "MexerDB"
 dbname <- "ScratchMDB"
 
 # Tells what CL-PFU dataset you are creating
@@ -95,7 +94,7 @@ clpfu_dataset <- "CL-PFU"
 clpfu_iea_dataset <- "CL-PFU IEA"
 clpfu_mw_dataset <- "CL-PFU MW"
 clpfu_both_dataset <- "CL-PFU IEA+MW"
-clpfu_version <- "v2.0"
+clpfu_version <- "v2.1a1"
 
 # Tells what IEAEWEB dataset you are using
 iea_dataset <- "IEA EWEB"
