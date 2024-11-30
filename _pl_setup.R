91598bc417ac92eb62efb7961dc6921a1f0dcd84
Matthew Heun <matthew.heun@me.com>
2024-11-30 15:22:25 -0500

    Debugging with NULL crew_controller

--- _pl_setup.R
+++ _pl_setup.R
@@ -73,12 +73,12 @@ if (parallel::detectCores() == 10) {
 }
 
 # For debugging in a single thread. Also set callr_function = NULL.
-# crew_controller <- NULL
+crew_controller <- NULL
 # For parallel processing.
-crew_controller <- crew::crew_controller_local(
-  workers = worker_threads,
-  seconds_idle = 60,
-  r_arguments = "--max-connections=512")
+# crew_controller <- crew::crew_controller_local(
+#   workers = worker_threads,
+#   seconds_idle = 60,
+#   r_arguments = "--max-connections=512")
 
 
 #
@@ -121,6 +121,7 @@ project_path <- file.path("~",
 # Think VERY CAREFULLY before setting this TRUE!
 reset_schema <- FALSE
 # Set back to FALSE quickly, i.e. immediately after tar_make()!
+# Likely the only need to set TRUE is after start_over().
 
 
 # In a typical setup, everything below this point
