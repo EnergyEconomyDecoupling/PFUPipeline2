ff5703a1f4b8c73954b8a0645fa7988295022853
Matthew Heun <matthew.heun@me.com>
2025-05-15 13:35:25 -0400

    Better defaults for start_over()

--- R/start_over.R
+++ R/start_over.R
@@ -10,13 +10,12 @@
 #' @param drv The database driver.
 #'            Default is `RPostgres::Postgres()`.
 #' @param dbname The name of the database you want nuke.
-#'               Default is "v2.0a1".
 #' @param host The database host.
 #'             Default is "eviz.cs.calvin.edu".
 #' @param port The database port.
 #'             Default is `6432`.
 #' @param user The database user.
-#'             Default is "postgres".
+#'             Default is "dbcreator".
 #' @param conn The database connection used internally.
 #'             Default is constructed from the
 #'             `drv`, `dbname`, `host`, `port`, and `user`
