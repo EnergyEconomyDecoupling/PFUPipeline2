83ad1cb57b24d4996c2d2c8573c94b4575a17e9f
Matthew Heun <matthew.heun@me.com>
2026-01-29 15:40:01 -0500

    Updates to pipeline

--- _pl_setup.R
+++ _pl_setup.R
@@ -48,7 +48,7 @@
 # Countries with updated allocations for industrial sectors
 # as of 29 Jan 2026
 countries <- c("ALB", "BEL", "BGR", "CHE", "CYP", "CZE", "DEU", "DNK", "EST", "GRC", "ESP",
-               "EURP", "FIN", "FRA", "HRV", "HUN", "IRL", "ITA", "LTU", "LUX",
+               "FIN", "FRA", "HRV", "HUN", "IRL", "ITA", "LTU", "LUX",
                "LVA", "MLT", "NLD", "POL", "PRT", "ROU", "SWE", "SWZ", "SVN", "SVK", "WRLD")
 
 # Additional exemplar countries are countries which aren't included in the workflow
