270203d4796e8fca68951518b2fb968f21ee502d
Matthew Heun <matthew.heun@me.com>
2026-05-18 08:38:52 -0400

    Prep for full run of pipeline

--- _pl_setup.R
+++ _pl_setup.R
@@ -13,7 +13,7 @@
 
 # Countries --------------------------------------------------------------------
 
-# countries <- c(PFUPipelineTools::canonical_countries, wrld = "WRLD") |> as.character()
+countries <- c(PFUPipelineTools::canonical_countries, wrld = "WRLD") |> as.character()
 # countries <- "AGO"
 # countries <- "BEN" # First country with no EIOU
 # countries <- "CMR"
@@ -113,7 +113,7 @@
 
 
 # Countries for versioning tests on 14 May 2026
-countries <- "AUS"
+# countries <- "AUS"
 
 
 
@@ -153,10 +153,10 @@ if (!("WRLD" %in% countries) & !("WRLD" %in% additional_exemplar_countries)) {
 
 # Years ------------------------------------------------------------------------
 
-# years <- 1960:2020
+years <- 1960:2020
 # years <- 1960
 # years <- 1971
-years <- 1996
+# years <- 1996
 # years <- 2010
 # years <- 2013
 # years <- 1960:1980
@@ -208,7 +208,7 @@ clpfu_iea_dataset <- "CL-PFU IEA"
 clpfu_mw_dataset <- "CL-PFU MW"
 clpfu_both_dataset <- "CL-PFU IEA+MW"
 # clpfu_version <- "v2.1a4"
-clpfu_version <- "v3.0a2"
+clpfu_version <- "v3.0a1"
 
 # Tells what IEAEWEB dataset you are using
 iea_dataset <- "IEA EWEB"
