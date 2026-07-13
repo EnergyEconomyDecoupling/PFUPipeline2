73d4b665d1bd4d5c879d01af1af30dd384502d80
MatthewHeun <matthew.heun@me.com>
2026-07-13 18:30:54 +0100

    Use new connection function. Now to v3.0a2.

--- report_scripts/Aramendia Emmanuel/Kaya paper data for Emmanuel.R
+++ report_scripts/Aramendia Emmanuel/Kaya paper data for Emmanuel.R
@@ -4,12 +4,11 @@
 # Upload to the OneDrive folder at
 # https://leeds365-my.sharepoint.com/personal/earear_leeds_ac_uk/_layouts/15/onedrive.aspx?e=5%3A895fe2332f3b4a16a6a84e4afcf07d58&sharingv2=true&fromShare=true&at=9&CID=09737963%2D17b4%2D46fb%2Da5c3%2De68f9a22b521&FolderCTID=0x012000292DEB6833DAEC48BD5C809FE95AA088&id=%2Fpersonal%2Fearear%5Fleeds%5Fac%5Fuk%2FDocuments%2FPFU%5FDatabase%5Fv2%5FKayaPaper
 
-conn <- PFUPipelineTools::get_db_conn(dbname = "ScratchMDB",
-                                      user = "dbcreator")
+conn <- PFUPipelineTools::get_scratchmdb_conn()
 on.exit(DBI::dbDisconnect(conn))
 
 
-v_string <- "v3.0a1"
+v_string <- "v3.0a2"
 
 Y_fu_U_EIOU_fu_details <- "YfuUEIOUfudetails" |>
   PFUPipelineTools::pl_filter_collect(version_string = v_string,
