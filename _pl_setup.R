93f219d609a3ed1ee93cca4c41bc4b48bd8ac8b9
Matthew Heun <matthew.heun@me.com>
2026-08-11 14:58:58 -0400

    Minor updates.

--- _pl_setup.R
+++ _pl_setup.R
@@ -7,8 +7,8 @@
 
 # Countries --------------------------------------------------------------------
 
-# countries <- c(PFUPipelineTools::canonical_countries, wrld = "WRLD") |> as.character()
-countries <- "GHA"
+countries <- c(PFUPipelineTools::canonical_countries, wrld = "WRLD") |> as.character()
+# countries <- c("DEU", "USA")
 
 # Regular: tar_make()
 # Debugging: tar_make(callr_function = NULL, use_crew = FALSE, as_job = FALSE)
@@ -44,8 +44,7 @@ if (!("WRLD" %in% countries) & !("WRLD" %in% additional_exemplar_countries)) {
 
 # Years ------------------------------------------------------------------------
 
-# years <- 1960:2020
-years <- 1996
+years <- 1960:2020
 
 # Set the years to provide exiobase coefficients
 years_exiobase <- 1995:2020
@@ -80,14 +79,14 @@ reset_schema <- FALSE
 
 # Tells what database you are targeting
 # dbname <- "ScratchMDB"
-dbname <- "SandboxDB"
+dbname <- "ScratchMDB"
 
 # Tells what CL-PFU dataset you are creating
 clpfu_dataset <- "CL-PFU"
 clpfu_iea_dataset <- "CL-PFU IEA"
 clpfu_mw_dataset <- "CL-PFU MW"
 clpfu_both_dataset <- "CL-PFU IEA+MW"
-clpfu_version <- "v3.0b2"
+clpfu_version <- "v3.0"
 
 # Tells what IEAEWEB dataset you are using
 iea_dataset <- "IEA EWEB"
@@ -108,8 +107,8 @@ if (parallel::detectCores() == 10) {
   worker_threads <- 8
 } else if (parallel::detectCores() == 24) {
   # M2 Ultra
-  worker_threads <- 16
-  # worker_threads <- 4
+  # worker_threads <- 16
+  worker_threads <- 4
 } else {
   # To be safe
   worker_threads <- 2
