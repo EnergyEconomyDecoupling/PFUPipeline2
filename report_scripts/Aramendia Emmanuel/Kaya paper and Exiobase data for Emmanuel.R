8581a81ce79cca2c235baa4fd1f4b428fa6b7ba1
Matthew Heun <matthew.heun@me.com>
2026-08-18 11:29:22 -0400

    Update script for Emmanuel

--- report_scripts/Aramendia Emmanuel/Kaya paper and Exiobase data for Emmanuel.R
+++ report_scripts/Aramendia Emmanuel/Kaya paper and Exiobase data for Emmanuel.R
@@ -1,24 +1,42 @@
 # These data are for Emmanuel Aramendia for the
 # Kaya identity paper.
 
-# Upload to the OneDrive folder at
-# https://leeds365-my.sharepoint.com/personal/earear_leeds_ac_uk/_layouts/15/onedrive.aspx?e=5%3A290cf82d24194866a4c562d0235bbe18&sharingv2=true&fromShare=true&at=9&CID=e67e3dfb%2D8a88%2D49ba%2Dab8a%2Db49e8a5bf6f7&id=%2Fpersonal%2Fearear%5Fleeds%5Fac%5Fuk%2FDocuments%2FDatasets%2FCL%2DPFU%2FPFU%5FDatabase%5Fv2%5FKayaPaper&FolderCTID=0x012000292DEB6833DAEC48BD5C809FE95AA088&view=0
+v_string <- "v3.0"
+output_folder <- file.path("~",
+                           "OneDrive - University of Leeds",
+                           "Fellowship 1960-2015 PFU database research",
+                           "Output Data",
+                           v_string,
+                           "For Emmanuel")
+if (!file.exists(output_folder)) {
+  dir.create(output_folder)
+}
 
 # Read and save Exiobase output files that remain local
 targets::tar_read(ExiobaseEftoXfMultipliers) |>
-  write.csv(file = "~/Desktop/For Emmanuel/Ef_to_Xf_multipliers.csv", row.names = FALSE)
+  write.csv(file = file.path(output_folder,
+                             "Ef_to_Xf_multipliers.csv"),
+            row.names = FALSE)
 
 targets::tar_read(ExiobaseEftoEuMultipliers) |>
-  write.csv(file = "~/Desktop/For Emmanuel/Ef_to_Eu_multipliers.csv", row.names = FALSE)
+  write.csv(file = file.path(output_folder,
+                             "Ef_to_Eu_multipliers.csv"),
+            row.names = FALSE)
 
 targets::tar_read(ExiobaseXftoXuMultipliers) |>
-  write.csv(file = "~/Desktop/For Emmanuel/Xf_to_Xu_multipliers.csv", row.names = FALSE)
+  write.csv(file = file.path(output_folder,
+                             "Xf_to_Xu_multipliers.csv"),
+            row.names = FALSE)
 
 targets::tar_read(ExiobaseEftoElossMultipliers) |>
-  write.csv(file = "~/Desktop/For Emmanuel/Ef_to_Eloss_multipliers.csv", row.names = FALSE)
+  write.csv(file = file.path(output_folder,
+                             "Ef_to_Eloss_multipliers.csv"),
+            row.names = FALSE)
 
 targets::tar_read(ExiobaseXftoXlossMultipliers) |>
-  write.csv(file = "~/Desktop/For Emmanuel/Xf_to_Xloss_multipliers.csv", row.names = FALSE)
+  write.csv(file = file.path(output_folder,
+                             "Xf_to_Xloss_multipliers.csv"),
+            row.names = FALSE)
 
 
 # Read data that are located in the Mexer database
@@ -26,14 +44,16 @@ conn <- PFUPipelineTools::get_scratchmdb_conn()
 on.exit(DBI::dbDisconnect(conn))
 
 
-v_string <- "v3.0b1"
+v_string <- "v3.0"
 
 Y_fu_U_EIOU_fu_details <- "YfuUEIOUfudetails" |>
   PFUPipelineTools::pl_filter_collect(version_string = v_string,
                                       conn = conn,
                                       collect = TRUE,
                                       matrix_class = "matrix")
-saveRDS(Y_fu_U_EIOU_fu_details, "~/Desktop/For Emmanuel/YfuUEIOUfudetails.rds")
+saveRDS(Y_fu_U_EIOU_fu_details,
+        file.path(output_folder,
+                  "YfuUEIOUfudetails.rds"))
 
 
 
@@ -42,7 +62,9 @@ eta_i <- "Etai" |>
                                       conn = conn,
                                       collect = TRUE,
                                       matrix_class = "matrix")
-saveRDS(eta_i, "~/Desktop/For Emmanuel/Etai.rds")
+saveRDS(eta_i,
+        file.path(output_folder,
+                  "Etai.rds"))
 
 
 
@@ -51,7 +73,9 @@ phi_vecs <- "Phivecs" |>
                                       conn = conn,
                                       collect = TRUE,
                                       matrix_class = "matrix")
-saveRDS(phi_vecs, "~/Desktop/For Emmanuel/Phivecs.rds")
+saveRDS(phi_vecs,
+        file.path(output_folder,
+                  "Phivecs.rds"))
 
 
 
@@ -60,7 +84,9 @@ agg_eta_pfu <- "AggEtaPFU" |>
                                       conn = conn,
                                       collect = TRUE,
                                       matrix_class = "matrix")
-saveRDS(agg_eta_pfu, "~/Desktop/For Emmanuel/AggEtaPFU.rds")
+saveRDS(agg_eta_pfu,
+        file.path(output_folder,
+                  "AggEtaPFU.rds"))
 
 
 
@@ -70,7 +96,9 @@ sector_agg_eta_fu <- "SectorAggEtaFU" |>
                                       conn = conn,
                                       collect = TRUE,
                                       matrix_class = "matrix")
-saveRDS(sector_agg_eta_fu, "~/Desktop/For Emmanuel/SectorAggEtaFU.rds")
+saveRDS(sector_agg_eta_fu,
+        file.path(output_folder,
+                  "SectorAggEtaFU.rds"))
 
 
 
@@ -92,7 +120,9 @@ mem.maxVSize(32768*2)
 
 # After that adjustment, saving to disk with the following line
 # was successful.
-saveRDS(psut_Re_all, "~/Desktop/For Emmanuel/PSUTReAll.rds")
+saveRDS(psut_Re_all,
+        file.path(output_folder,
+                  "PSUTReAll.rds"))
 
 # Set back to original size
 mem.maxVSize(32768)
