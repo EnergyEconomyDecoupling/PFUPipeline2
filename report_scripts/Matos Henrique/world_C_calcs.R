d83cadde982b1f16471f3c6fd632122cd9484dbb
Matthew Heun <matthew.heun@me.com>
2026-09-26 11:41:27 -0400

    C_Y calcs now working yet! But saving for now.

--- report_scripts/Matos Henrique/world_C_calcs.R
+++ report_scripts/Matos Henrique/world_C_calcs.R
@@ -7,6 +7,9 @@
 # from available information (Y_f and efficiencies)
 # for a particular country and year (GHA, 1971).
 
+# Setup ------------------------------------------------------------------------
+library(magrittr)
+
 # First, gather information from the database ----------------------------------
 conn <- PFUPipelineTools::get_scratchmdb_conn()
 on.exit(DBI::dbDisconnect(conn))
@@ -43,45 +46,47 @@ gha1971etavecs <- PFUPipelineTools::pl_filter_collect(version_string = "v3.0",
                                                       conn = conn)
 DBI::dbDisconnect(conn)
 
-Y_f <- gha1971 |>
+Y_f_db <- gha1971 |>
   dplyr::filter(LastStage == "Final") |>
   purrr::pluck("Y", 1) |>
   as.matrix()
-Y_f_vec <- Y_f |>
+Y_f_vec_db <- Y_f_db |>
   matsbyname::vectorize_byname(notation = RCLabels::arrow_notation)
-Y_f_vec_hat <- Y_f_vec |>
+Y_f_vec_hat_db <- Y_f_vec_db |>
   matsbyname::hatize_byname(keep = "rownames")
-Y_u <- gha1971 |>
+Y_f_vec_hat_inv_db <- Y_f_vec_db |>
+  matsbyname::hatinv_byname()
+Y_u_db <- gha1971 |>
   dplyr::filter(LastStage == "Useful") |>
   purrr::pluck("Y", 1) |>
   as.matrix()
-eta_vec <- gha1971etavecs$etafu[[1]] |>
+eta_vec_db <- gha1971etavecs$etafu[[1]] |>
   as.matrix()
-eta_vec_hat <- eta_vec |>
+eta_vec_hat_db <- eta_vec_db |>
   matsbyname::hatize_byname(keep = "rownames")
-C_Y <- gha1971cmats$C_Y[[1]] |>
+eta_vec_hat_inv_db <- eta_vec_db |>
+  matsbyname::hatinv_byname()
+C_Y_db <- gha1971cmats$C_Y[[1]] |>
   as.matrix()
 
+allocated_final <- matsbyname::matrixproduct_byname(Y_f_vec_hat_db, C_Y_db)
 
-allocated_final <- matsbyname::matrixproduct_byname(Y_f_vec_hat, C_Y)
-
-
-Y_u_calculated <- matsbyname::matrixproduct_byname(allocated_final, eta_vec_hat) |>
-  matsbyname::transpose_byname() %>%
-  matsbyname::setrownames_byname(RCLabels::switch_notation(rownames(.),
+Y_u_calc <- matsbyname::matrixproduct_byname(allocated_final, eta_vec_hat_db) %>%
+  matsbyname::setcolnames_byname(RCLabels::switch_notation(colnames(.),
                                                            from = RCLabels::arrow_notation,
                                                            to = RCLabels::from_notation,
                                                            flip = TRUE,
                                                            inf_notation = FALSE)) |>
   matsbyname::aggregate_to_pref_suff_byname(keep = "suff",
-                                            margin = 2,
+                                            margin = 1,
                                             notation = RCLabels::arrow_notation) |>
+  matsbyname::transpose_byname() |>
   matsbyname::clean_byname() |>
   matsbyname::sort_rows_cols()
 
 # This is the Y_u we want to test against, one that does NOT have
 # final energy going into sectors.
-Y_u_test <- Y_u |>
+Y_u_db <- Y_u_db |>
   matsbyname::select_rows_byname(remove_pattern =
                                    RCLabels::make_or_pattern(c("Aviation gasoline",
                                                                "Crude oil",
@@ -94,46 +99,36 @@ Y_u_test <- Y_u |>
 
 # The fact that this test returns TRUE shows that we can
 # Calculate Y_u from Y_f, C_Y, and etafu.
-# This also means we can reverse the process
+# The goal is now to reverse the process
 # to calculate C_Y from Y_u, Y_f, and etafu.
-matsbyname::equal_byname(Y_u_test, Y_u_calculated, tol = 1e-10)
+matsbyname::equal_byname(Y_u_db, Y_u_calc, tol = 1e-10)
 
 
 # Calculate C_Y from  Y_u, Y_f, and etafu. -------------------------------------
 
+# Try it with one country.
+# We can compare the result against the known C_Y matrix.
 
+Y_u_db_store <- Y_u_db |>
+  matsbyname::switch_notation_byname(margin = 1,
+                                     from = RCLabels::from_notation,
+                                     to = RCLabels::arrow_notation,
+                                     flip = TRUE) |>
+  matsbyname::transpose_byname()
 
+Y_u_db_expanded <-matsbyname::mat_from_store_byname(
+  a = Y_f_vec_hat_inv_db,
+  v = Y_u_db_store,
+  margin = 2,
+  margin_v = 1,
+  notation = RCLabels::arrow_notation,
+  a_piece = "suff",
+  v_piece = "all")
 
+C_Y_calc <- matsbyname::matrixproduct_byname(Y_f_vec_hat_inv_db, Y_u_db_expanded) |>
+  matsbyname::matrixproduct_byname(eta_vec_hat_inv)
 
-
-
-
-gha1971 |>
-  dplyr::mutate(
-    WorksheetNames = paste0(LastStage, "+", EnergyType)
-  ) |>
-  Recca::write_ecc_to_excel(path = "~/Desktop/GHA1971.xlsx",
-                            worksheet_names = "WorksheetNames",
-                            overwrite_file = TRUE)
-
-
-
-
-gha1971cmats$C_Y[[1]] |>
-  as.matrix() |>
-  openxlsx2::write_xlsx(file = "~/Desktop/CY.xlsx", row_names = TRUE)
-
-
-
-
-
-
-
-gha1971etavecs$etafu[[1]] |>
-  as.matrix() |>
-  openxlsx2::write_xlsx(file = "~/Desktop/eta.xlsx", row_names = TRUE)
-
-
+matsbyname::equal_byname(C_Y_calc, C_Y)
 
 
 
